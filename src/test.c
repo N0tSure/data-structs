d81c601bef0427d5770655099e7b950d62eebbc4
Artyom Sirosh <ASir2089@gmail.com>
2017-07-08 01:22:08 +0300

    Stack functional tested

--- src/test.c
+++ src/test.c
@@ -1,33 +1,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "object.h"
-#include "linked_list.h"
+#include "stack.h"
 
-void printList(node* list);
-
-int main()
+int main(int argc, char* argv[])
 {
-	int foes_size = 5;
-    char* foes[5] = { "foo", "bar", "baz", "qux", "quux" };
     
-	create(foes[0]);
-	for (int i = 1; i < foes_size; i++)
+	for (int i = 1; i < argc; ++i)
 	{
-		insert(foes[i]);
+		push(argv[i]);
 	}
-	
-	printf("Contains foo: %s\n", contains(foes[0]) ? "yes" : "no");
-	printf("Contains baz: %s\n", contains("baz") ? "yes" : "no");
-	printf("Contains qux before remove: %s\n", contains("qux") ? "yes" : "no");
-	printf("Contains corge: %s\n", contains("corge") ? "yes" : "no");
-	printf("Contains NULL: %s\n", contains(NULL) ? "yes" : "no");
-	
-	removeElement("foo");
-	removeElement("quux");
-	
-	printf("Contains after clear foo: %s\n", contains(foes[0]) ? "yes" : "no");
-	printf("Contains after clear baz: %s\n", contains("baz") ? "yes" : "no");
-	printf("Contains after clear quux: %s\n", contains("quux") ? "yes" : "no");
-    
+
+    char* s;
+    while ((s = pop()) != NULL)
+    {
+    	printf("pop: %s\n", s);
+    }
+
     return 0;
 }
