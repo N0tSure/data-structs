c3b145b441b1522a068a6a6fc7f3cf6ac65fd199
N0tSure <ASir2089@gmail.com>
2017-07-10 18:50:52 +0300

    * iterator and set added to Makefile  * linked list and iterator tested

--- src/test.c
+++ src/test.c
@@ -1,21 +1,30 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include "object.h"
-#include "stack.h"
+#include "linked_list.h"
+#include "iterator.h"
+#include "set.h"
 
 int main(int argc, char* argv[])
 {
-    
-	for (int i = 1; i < argc; ++i)
-	{
-		push(argv[i]);
+    node* list = NULL;
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++)
+		{
+			list = insert(argv[i], list);
+			printf("%s ", argv[i]);
+		}
+		printf("\n");
+		
+		char s[10];
+		scanf("%s", s);
+		printf("%s : %c\n", s, removeElement(s, list) ? 'y' : 'n');
 	}
-
-    char* s;
-    while ((s = pop()) != NULL)
-    {
-    	printf("pop: %s\n", s);
-    }
+	
+	iterator(list);
+	while(hasNext())
+		printf("%s ", next());
+	
+	printf("\n");
 
     return 0;
 }
